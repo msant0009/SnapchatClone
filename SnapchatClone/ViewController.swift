eb2b342d340484eea9512e875ee1ca920f960cb5
Mark Santoro <mark_santoro@comcast.net>
2024-08-17 09:38:47 -0700

    Initial Commit

---
+++ SnapchatClone/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  SnapchatClone
+//
+//  Created by Mark Santoro on 8/17/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
