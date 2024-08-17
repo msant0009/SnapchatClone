a7b298c72d20b767fd2a3363ee14ec8532830054
Mark Santoro <mark_santoro@comcast.net>
2024-08-17 10:26:38 -0700

    view controller setup done

---
+++ SnapchatClone/SignInVC.swift
@@ -0,0 +1,34 @@
+//
+//  ViewController.swift
+//  SnapchatClone
+//
+//  Created by Mark Santoro on 8/17/24.
+//
+
+import UIKit
+
+class SignInVC: UIViewController {
+
+    
+    @IBOutlet var emailText: UITextField!
+    @IBOutlet var nameText: UITextField!
+    @IBOutlet var passwordText: UITextField!
+    
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+    @IBAction func signInClicked(_ sender: Any) {
+    }
+    
+    
+    @IBAction func signUpClicked(_ sender: Any) {
+    }
+    
+    
+    
+}
+
