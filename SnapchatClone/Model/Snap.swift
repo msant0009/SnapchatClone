1a7e0fdc9e6b90985ab597d586462d9f8483b379
Mark Santoro <mark_santoro@comcast.net>
2024-08-20 08:09:23 -0700

    add Snap model file, add initial data read code to FeedVC

---
+++ SnapchatClone/Model/Snap.swift
@@ -0,0 +1,16 @@
+//
+//  Snap.swift
+//  SnapchatClone
+//
+//  Created by Mark Santoro on 8/20/24.
+//
+
+import Foundation
+
+struct Snap {
+    var username : String
+    var imageUrlArray : [String]
+    var date : Date
+    
+    
+}
--- SnapchatClone/ViewControllers/FeedVC.swift
+++ SnapchatClone/ViewControllers/FeedVC.swift
@@ -15,6 +15,7 @@ class FeedVC: UIViewController, UITableViewDelegate, UITableViewDataSource {
     @IBOutlet var tableView: UITableView!
   
     let fireStoreDatabase = Firestore.firestore()
+    var snapArray = [Snap]()
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -33,10 +34,36 @@ class FeedVC: UIViewController, UITableViewDelegate, UITableViewDataSource {
                 self.makeAlert(title: "Error", message: error?.localizedDescription ?? "Error!")
             } else{
                 if snapshot?.isEmpty == false && snapshot != nil {
+                    self.snapArray.removeAll(keepingCapacity: false)
                     
                     for document in snapshot!.documents{
                         
+                        let documentId = document.documentID
                         
+                        if let username = document.get("snapOwner") as? String {
+                            if let imageUrlArray = document.get("imageUrlArray") as? [String] {
+                                if let date = document.get("date") as? Timestamp {
+                                    
+                                    if let difference = Calendar.current.dateComponents([.hour], from: date.dateValue(), to: Date()).hour {
+                                        if difference >= 24{
+                                            //delete
+                                            self.fireStoreDatabase.collection("Snaps").document(documentId).delete() {(error) in
+                                                
+                                                
+                                            }
+                                        }
+                                        // Time left to deletion --> Snap.VC
+                                        
+                                    }
+                                        
+                                    
+                                    let snap = Snap(username: username, imageUrlArray: imageUrlArray, date: date.dateValue())
+                                    self.snapArray.append(snap)
+                                    
+                                    
+                                }
+                            }
+                        }
                         
                     }
                 }
@@ -74,12 +101,12 @@ class FeedVC: UIViewController, UITableViewDelegate, UITableViewDataSource {
     }
 
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return 10
+        return snapArray.count
     }
     
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         let cell = tableView.dequeueReusableCell(withIdentifier: "Cell", for: indexPath) as! FeedCell
-        cell.feedUserNameLabel.text = "test"
+        cell.feedUserNameLabel.text = snapArray[indexPath.row].username
         return cell
     }
     
